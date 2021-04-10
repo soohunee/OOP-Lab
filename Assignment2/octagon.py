a11e556219ff664b264969679424ed2d596cae9d
Soohun Park <76514241+soohunee@users.noreply.github.com>
2021-04-10 23:39:12 +0900

    Assignment2

---
+++ Assignment2/octagon.py
@@ -0,0 +1,30 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.forward(100)
+t.right(45)
+
+t.forward(100)
+t.right(45)
+
+t.forward(100)
+t.right(45)
+
+t.forward(100)
+t.right(45)
+
+t.forward(100)
+t.right(45)
+
+t.forward(100)
+t.right(45)
+
+
+t.forward(100)
+t.right(45)
+
+t.forward(100)
+t.right(45)
+
+turtle.done()
